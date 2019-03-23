f7b6b8a440e54c1e8f84a3e4380423fe2c987d86
Kelson Warner <kelson@null.net>
2019-03-23 13:18:06 -0700

    added second portfolio item

--- src/Works/index.js
+++ src/Works/index.js
@@ -1,36 +1,69 @@
 import * as React from 'react';
 import warbler from './warbler.png';
+import shareshot from './shareshot.png';
 
 import './Works.scss';
 
 function Works() {
   return (
     <div className="Works">
-      <div>
+      <div className="Warbler">
         <img
-          className="warbler-a"
+          className="Warbler__image"
           src={warbler}
           alt="Warbler, a twitter clone project"
         />
+        <div className="Warbler__info">
+          <p className="Warbler__description">
+            Warbler, a twitter clone built with <i>Python (flask)</i>
+          </p>
+          <a className="Warbler__link">github</a>|
+          <a className="Warbler__link">site</a>
+        </div>
       </div>
-      <div>
+
+      <div className="Shareshot">
+        <div className="Shareshot__info">
+          <p className="Sharehot__description">
+            Shareshot, a modern image sharing app built with VueJS, GraphQL,
+            Apollo, and Express
+          </p>
+          <a className="Shareshot__link">github</a>|
+          <a className="Shareshot__link">site</a>
+        </div>
         <img
-          className="warbler-c"
-          src={warbler}
+          className="Shareshot__image"
+          src={shareshot}
           alt="Warbler, a twitter clone project"
         />
       </div>
-      <div>
+      <div className="Warbler">
         <img
-          className="warbler-c"
+          className="Warbler__image"
           src={warbler}
           alt="Warbler, a twitter clone project"
         />
+        <div className="Warbler__info">
+          <p className="Warbler__description">
+            Warbler, a twitter clone built with <i>Python (flask)</i>
+          </p>
+          <a className="Warbler__link">github</a>|
+          <a className="Warbler__link">site</a>
+        </div>
       </div>
-      <div>
+
+      <div className="Shareshot">
+        <div className="Shareshot__info">
+          <p className="Sharehot__description">
+            Shareshot, a modern image sharing app built with VueJS, GraphQL,
+            Apollo, and Express
+          </p>
+          <a className="Shareshot__link">github</a>|
+          <a className="Shareshot__link">site</a>
+        </div>
         <img
-          className="warbler-b"
-          src={warbler}
+          className="Shareshot__image"
+          src={shareshot}
           alt="Warbler, a twitter clone project"
         />
       </div>
