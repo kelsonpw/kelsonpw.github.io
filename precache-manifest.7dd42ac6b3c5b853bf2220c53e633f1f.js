9005e5a21a4377ccb42e963293100da559a789ab
Kelson Warner <kelson@null.net>
2019-03-24 11:56:19 -0700

    Updates

---
+++ precache-manifest.7dd42ac6b3c5b853bf2220c53e633f1f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d79ec28e4f8fa38fa4b",
+    "url": "/kelsonpw.github.io/static/css/main.c840e719.chunk.css"
+  },
+  {
+    "revision": "6d79ec28e4f8fa38fa4b",
+    "url": "/kelsonpw.github.io/static/js/main.72e4d44e.chunk.js"
+  },
+  {
+    "revision": "2d35de7919e286436f4c",
+    "url": "/kelsonpw.github.io/static/js/runtime~main.6006b1d9.js"
+  },
+  {
+    "revision": "2edd9084cd6ee7b4e4d9",
+    "url": "/kelsonpw.github.io/static/css/2.36d9285c.chunk.css"
+  },
+  {
+    "revision": "2edd9084cd6ee7b4e4d9",
+    "url": "/kelsonpw.github.io/static/js/2.795291b4.chunk.js"
+  },
+  {
+    "revision": "3972566e50c168ca26a024e140fdb5b8",
+    "url": "/kelsonpw.github.io/static/media/kelson-warner-resume.3972566e.pdf"
+  },
+  {
+    "revision": "07a2fc0f609f961782af58b092058cd6",
+    "url": "/kelsonpw.github.io/static/media/airhead.07a2fc0f.png"
+  },
+  {
+    "revision": "36e0a0470cb21467d02f2a9192053b91",
+    "url": "/kelsonpw.github.io/static/media/shareshot.36e0a047.png"
+  },
+  {
+    "revision": "03f12b33019dc4de13d0f814848853a0",
+    "url": "/kelsonpw.github.io/static/media/warbler.03f12b33.png"
+  },
+  {
+    "revision": "d3705196530a940ed5cc79ba867a6d88",
+    "url": "/kelsonpw.github.io/static/media/yahtzee.d3705196.png"
+  },
+  {
+    "revision": "f694c857d5d2b6b270018dbf4eef5cfa",
+    "url": "/kelsonpw.github.io/static/media/headshot.f694c857.png"
+  },
+  {
+    "revision": "f7417b87133d2dee6fcaf2379c18178a",
+    "url": "/kelsonpw.github.io/index.html"
+  }
+];
