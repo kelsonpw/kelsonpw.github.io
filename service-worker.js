d7721a7ee3591384947c175a8cc7a0c1b5f26454
Kelson Warner <kelson@null.net>
2019-03-24 12:06:52 -0700

    updated build

--- service-worker.js
+++ service-worker.js
@@ -11,11 +11,11 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-
 importScripts(
-  "/precache-manifest.bbbe5c97f44099109ccc2d0e37fd73cc.js"
+  'https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js'
 );
+
+importScripts('/precache-manifest.7dd42ac6b3c5b853bf2220c53e633f1f.js');
 
 workbox.clientsClaim();
 
@@ -28,7 +28,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
-  
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+workbox.routing.registerNavigationRoute('/index.html', {
+  blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
 });
